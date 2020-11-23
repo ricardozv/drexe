78fd5f197b6adb3b0d291ecca8a4152b6dc5dffa
ricardozv <ricardozv@gmail.com>
2020-11-23 19:16:11 -0300

    up

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'ricardozv',
+    password: '#$Kank181983',
+    database: 'marapay',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+
+};
