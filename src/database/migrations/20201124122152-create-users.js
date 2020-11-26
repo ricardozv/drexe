c939c88c7a3c7ebde80aca3dec03c699001d23b2
ricardozv <ricardozv@gmail.com>
2020-11-26 19:18:30 -0300

    update migrations

--- src/database/migrations/20201124122152-create-users.js
+++ src/database/migrations/20201124122152-create-users.js
@@ -5,34 +5,68 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.createTable('users', { 
+      
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true, 
         primaryKey: true,
       },
+
       name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
+      cpf: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+
+      rg: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+
       email: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
       },
+
+      adress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
       password_hash: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
+      photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      balance: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
       provider: {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
         allowNull: false,
       },
+
       create_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
+
       updated_at:{
         type: Sequelize.DATE,
         allowNull: false,
